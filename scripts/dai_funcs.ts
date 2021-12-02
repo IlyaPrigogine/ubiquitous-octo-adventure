360ecd7c181ec501ac27285193f5e4c8827388fc
IlyaPrigogine <prigogine@protonmail.com>
2021-12-02 17:18:59 +0800

    Update dai_funcs.ts

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -1,11 +1,15 @@
 import {deployments, ethers, getNamedAccounts} from 'hardhat';
-import {Greeter} from "../typechain";
+import {Greeter, MyDefiProject} from "../typechain";
 
 const {execute, read} = deployments;
 
 async function main() {
 
     const {owner} = await getNamedAccounts();
+
+    const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
+    console.log(`MyDefiProject.address: ${await MyDefiProject.address}`);
+
 
 }
 
